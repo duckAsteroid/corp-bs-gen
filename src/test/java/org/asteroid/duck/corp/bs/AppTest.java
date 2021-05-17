8d78d2de71485a5f16dc05d04366b3cfddec1d46
chris <chris.senior@cognitran.com>
2021-05-17 21:03:08 +0100

    Updated dependencies and unit/integration test

--- src/test/java/org/asteroid/duck/corp/bs/AppTest.java
+++ src/test/java/org/asteroid/duck/corp/bs/AppTest.java
@@ -4,11 +4,38 @@
 package org.asteroid.duck.corp.bs;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {Controller.class, App.class})
+@WebMvcTest
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Test
+    void appHasAGreeting() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/rest/bs")
+                .accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String message = result.getResponse().getContentAsString();
+        assertNotNull(message);
+        System.out.println(message);
     }
 }
